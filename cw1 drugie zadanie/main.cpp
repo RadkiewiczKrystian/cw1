d69dff78ef26e1d136b5444b29b698773f023b0f
RadkiewiczKrystian <krystianr174@wp.pl>
2017-09-25 15:36:39 +0200

    123

--- cw1 drugie zadanie/main.cpp
+++ cw1 drugie zadanie/main.cpp
@@ -8,10 +8,12 @@ int main(void)
 
    int rok_urodzenia;
    int aktualny_rok =2017;
-   int wiek =aktualny_rok - rok_urodzenia;
+
   cout<< "Podaj rok urodzenia"<<endl;
   cin>> rok_urodzenia;
   cout<<" Twoj rok urodzenia to: " <<rok_urodzenia<<endl;
+  int wiek =aktualny_rok - rok_urodzenia;
+  cout<<" Aktualnie masz:"<< wiek<<" "<<"lat"<<endl;
 
 
     return 0;
