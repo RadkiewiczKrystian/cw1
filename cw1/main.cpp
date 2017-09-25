f884a8c9d62a2fff6f7b5f87f0739ee14d47dc57
RadkiewiczKrystian <krystianr174@wp.pl>
2017-09-25 15:27:42 +0200

    cw1

---
+++ cw1/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(void)
+{
+
+
+   int rok_urodzenia = 1997;
+   int aktualny_rok = 2017;
+   int wiek =aktualny_rok - rok_urodzenia;
+   string imie = "Krystian";
+   string nazwisko ="Radkiewicz";
+   cout<< "Witaj"<<endl;
+   cout<<"Witam, nazywam sie" << imie<<" "<<nazwisko<<endl;
+    cout<<"i mam "<<wiek<<" "<<"lat"<< endl;
+    return 0;
+}
